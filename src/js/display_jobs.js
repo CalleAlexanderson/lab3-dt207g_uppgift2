294b92614db1c4c3089c512e91a6b3f7567ac72f
CalleAlexanderson <calle.alexanderson@telia.com>
2024-04-21 21:16:53 +0200

    uppdaterat displayworks så det funkar med ny server

--- src/js/display_jobs.js
+++ src/js/display_jobs.js
@@ -11,7 +11,7 @@ window.addEventListener('load', init);
 async function getWorks() {
     let workplaces;
     try {
-        const response = await fetch(`http://127.0.0.1:3000/api/workplaces`);
+        const response = await fetch(`http://127.0.0.1:3000/workposts`);
         workplaces = await response.json();
     } catch (error) {
         console.log(error);
@@ -22,25 +22,29 @@ async function getWorks() {
 
 function displayWorks(works) {
     let worksDiv = document.getElementById('works_div');
+    console.log(works[0]);
     worksDiv.innerHTML = "";
     for (let index = 0; index < works.length; index++) {
         let art = document.createElement("article");
         let jobtitle = document.createElement("h3");
-        jobtitle.innerHTML = works[index].jobtitle;
+        jobtitle.innerHTML = works[index].jobTitle;
         let company = document.createElement("p");
-        company.innerHTML = "Företag: "+works[index].companyname;
+        company.innerHTML = "Företag: "+works[index].companyName;
         let loc = document.createElement("p");
         loc.innerHTML = "Plats: "+works[index].location;
         let desc = document.createElement("p");
-        desc.innerHTML = works[index].description;
+        if (works[index].description != undefined) {
+            desc.innerHTML = "Arbetsbeskrivning: "+works[index].description;
+        } else {
+            desc.innerHTML = "Ingen arbetsbeskrivning";
+        }
         desc.classList.add("artDesc");
         let btnDelete = document.createElement("button");
         btnDelete.innerHTML = "Ta bort jobbpost";
-        btnDelete.id = works[index].ID;
+        btnDelete.id = works[index]._id;
         btnDelete.addEventListener("click",  ()=>{
             deleteWorks(btnDelete.id)
         })
-        // aDelete.href = `/api/workplaces/:${works[index].ID}`;
         
         art.appendChild(jobtitle);
         art.appendChild(company);
@@ -52,12 +56,12 @@ function displayWorks(works) {
     }
 }
 
-async function deleteWorks(id) {
-    const response = await fetch(`http://127.0.0.1:3000/api/workplaces/${id}`, {
-        method: "DELETE"
-    });
+// async function deleteWorks(id) {
+//     const response = await fetch(`http://127.0.0.1:3000/workposts/${id}`, {
+//         method: "DELETE"
+//     });
 
-    const res = await response.json();
-    console.log(res);
-    getWorks();
-}
+//     const res = await response.json();
+//     console.log(res);
+//     getWorks();
+// }
